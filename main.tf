acb6fa8c0b75ec8491bcd8e05c6b6098510aa69e
Adrienn Szocs <adrienn.szocs@gmail.com>
2026-01-14 18:10:50 +0100

    6

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "azurerm_network_security_group" "rg" {
 
 # Create a virtual network
 resource "azurerm_virtual_network" "vnet1" {
-  name                = "myTFVnet"
+  name                = "myNewTFVnet"
   address_space       = ["10.0.0.0/16"]
   location            = azurem_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
